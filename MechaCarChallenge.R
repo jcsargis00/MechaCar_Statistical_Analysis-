7fca2312d9891aa2b377127dd225a695697e1698
jcsargis00 <79231870+jcsargis00@users.noreply.github.com>
2022-04-11 22:30:07 -0700

    update with correct mu

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -10,15 +10,17 @@ summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_cleara
 # import Suspension_Coil.csv as a dataframe
 Suspension_Coil <- read.csv('Suspension_Coil.csv', check.names=F,stringsAsFactors = F)
 head(Suspension_Coil)
+# total summary
 total_summary <- Suspension_Coil %>% summarize(Mean = mean(PSI), Median = median(PSI), Variance = var(PSI), SD = sd(PSI), .groups = 'keep')
+# lot summary
 lot_summary <- Suspension_Coil %>% group_by(Manufacturing_Lot) %>% summarize(Mean = mean(PSI),Median = median(PSI), Variance = var(PSI), SD = sd(PSI) , .groups = 'keep') 
 # T-tests for all lots and individual lots, mu =1500 psi
 t.test(Suspension_Coil$PSI,mu=mean(Suspension_Coil$PSI))
 # lot 1
-t.test(subset(Suspension_Coil$PSI,Suspension_Coil$Manufacturing_Lot == "Lot1"),mu=1500)
+t.test(subset(Suspension_Coil$PSI,Suspension_Coil$Manufacturing_Lot == "Lot1"),mu=mean(Suspension_Coil$PSI))
 # lot 2
-t.test(subset(Suspension_Coil$PSI,Suspension_Coil$Manufacturing_Lot == "Lot2"),mu=1500)
+t.test(subset(Suspension_Coil$PSI,Suspension_Coil$Manufacturing_Lot == "Lot2"),mu=mean(Suspension_Coil$PSI))
 # lot 3
-t.test(subset(Suspension_Coil$PSI,Suspension_Coil$Manufacturing_Lot == "Lot3"),mu=1500)
+t.test(subset(Suspension_Coil$PSI,Suspension_Coil$Manufacturing_Lot == "Lot3"),mu=mean(Suspension_Coil$PSI))
 
 
